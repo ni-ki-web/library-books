49d14343cecb262099da08c279b7823cf2f25305
ni-ki-web <198837997+ni-ki-web@users.noreply.github.com>
2025-08-31 16:45:31 +1200

    Add Book constructor, newBook instance/objects, and the funtion to add the book to library

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+
+}
+
+function toggleReadStatus() {
+
+}
