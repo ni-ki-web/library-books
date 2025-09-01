de3eb0d6d45c5612b9e722733ec7ed2a7015dc63
ni-ki-web <198837997+ni-ki-web@users.noreply.github.com>
2025-09-02 11:25:55 +1200

    Update: created a library to store the books in, created a Book Constructor, added a toggle button to switch read status, create book objects and push them to the library

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+// Book Constructor
+function Book(title, author, pages, genre, read, imageUrl="", imageFile) {
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.genre = genre;
     this.read = read;
+    this.imageUrl = imageUrl;
+    this.imageFile = imageFile;
 }
 
-function addBookToLibrary() {
-
+//Toggle Method to switch read status
+Book.prototype.toggleReadStatus = function() {
+    this.read = !this.read;
 }
 
-function toggleReadStatus() {
-
+// Create book objects and add them to the library
+function addBookToLibrary(book) {
+    const book = new Book(title, author, pages, genre, read, imageUrl, imageFile);
+    myLibrary.push(book);
+    return book;
 }
