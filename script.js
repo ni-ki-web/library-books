361c8b13750be46ad6fbeb781674fdacde309508
ni-ki-web <198837997+ni-ki-web@users.noreply.github.com>
2025-09-02 12:04:33 +1200

    Update: build a book card for each book; if there are no books in the library show a message

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const library = document.getElementById("library");
+
 const myLibrary = [];
 
 // Book Constructor
@@ -18,8 +20,54 @@ Book.prototype.toggleReadStatus = function() {
 }
 
 // Create book objects and add them to the library
-function addBookToLibrary(book) {
+function addBookToLibrary(title, author, pages, genre, read, imageUrl, imageFile) {
     const book = new Book(title, author, pages, genre, read, imageUrl, imageFile);
     myLibrary.push(book);
     return book;
+}
+
+function buildBookCard(book) {
+    const bookCard = document.createElement("div");
+    bookCard.className = "book-card";
+    bookCard.dataset.id = book.id;
+
+    const imgWrapper = document.createElement("div");
+    imgWrapper.className = "book-cover";
+
+    const img = document.createElement("img");
+    img.src = book.imageUrl || "assets/placeholder-cover.png" || "https://indigenousreadsrisingcom.b-cdn.net/wp-content/uploads/2023/10/1.png";
+    img.alt = `${book.title}cover`;
+    imgWrapper.appendChild(img);
+
+    const title = document.createElement("h3");
+    title.textContent = book.title;
+
+    const author = document.createElement("p");
+    author.className = "book-meta";
+    author.textContent = book.author;
+
+    const pages = document.createElement("p");
+    pages.className = "book-meta";
+    pages.textContent = book.pages;
+
+    const genre = document.createElement("p");
+    genre.className = "book-meta";
+    genre.textContent = book.genre;
+
+    bookCard.appendChild(imgWrapper);
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(genre);
+}
+
+function displayBook() {
+    library.textContent = "";
+
+    if(myLibrary.length === 0) {
+        const emptyLibrary = document.createElement("p");
+        emptyLibrary.textContent = "No book in the library. Add your first one!"
+        library.appendChild(emptyLibrary);
+        return;
+    }
 }
